7a2cfca020e7c8bb8cee0fca5b7adec1f847528e
Safwen MARZOUGUI <safwen@gmail.com>
2015-12-04 21:58:22 +0100

    Creating a config file.

---
+++ app/src/config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by smu on 04/12/2015.
+ */
+
+'use strict';
+
+angular
+    .module('crmDemo', ['ngMaterial', 'users'])
+    .config(function($mdThemingProvider, $mdIconProvider) {
+
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+            .icon("twitter", "./assets/svg/twitter.svg", 512)
+            .icon("phone", "./assets/svg/phone.svg", 512);
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+
+    });
