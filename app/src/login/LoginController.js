1320a3014b864e3dc022f29e2997a554e06565d2
Safwen MARZOUGUI <safwen@gmail.com>
2015-12-05 01:50:41 +0100

    Making simple authentication to FireBase with login/pass  OK !

--- app/src/login/LoginController.js
+++ app/src/login/LoginController.js
@@ -8,8 +8,30 @@
         $scope = scope;
         $firebaseSimpleLogin = firebaseSimpleLogin;
 
+        $scope.user = {};
+
         var firebaseObj = new Firebase("https://luminous-fire-4441.firebaseio.com");
         var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+
+        $scope.SignIn = function(event) {
+            event.preventDefault();  // To prevent form refresh
+            var username = $scope.user.email;
+            var password = $scope.user.password;
+
+            loginObj.$login('password', {
+                email: username,
+                password: password
+            })
+                .then(function(user) {
+                    // Success callback
+                    console.log('Authentication successful');
+                }, function(error) {
+                    // Failure callback
+                    console.log('Authentication failure');
+                });
+        }
+
         console.log (loginObj);
 
     }
