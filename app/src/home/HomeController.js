c9df432df376a02bbbf94748602f28249e432215
Safwen MARZOUGUI <safwen@gmail.com>
2015-12-06 21:38:02 +0100

    Adding the shycronization.

--- app/src/home/HomeController.js
+++ app/src/home/HomeController.js
@@ -5,9 +5,9 @@ var $firebaseAuth;
 
 
 angular.module('crmDemo.home').controller('HomeController', LoginController);
-LoginController.$inject = ['$scope', '$firebaseAuth', 'auth', 'store', '$firebaseArray'];
+LoginController.$inject = ['$scope', '$firebaseAuth', 'auth', 'store', '$firebaseArray', '$firebaseObject'];
 
-function LoginController($scope, firebaseAuth, auth, store, $firebaseArray) {
+function LoginController($scope, firebaseAuth, auth, store, $firebaseArray, $firebaseObject) {
 
     $firebaseAuth = firebaseAuth;
     $scope.auth = auth;
@@ -16,27 +16,27 @@ function LoginController($scope, firebaseAuth, auth, store, $firebaseArray) {
     $scope.user = {};
 
 
-    // Grab the token
-    var firebaseToken = store.get('firebaseToken');
+    var friendsRef = new Firebase("https://luminous-fire-4441.firebaseio.com/days");
+    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NybS1kZW1vLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMjczMjM3MjI2ODg5MDM4NTIxMyIsImF1ZCI6InA1b3F1MXhjNFU3QThrM3ZpbVdpVjd0QXBsTkxONlp6IiwiZXhwIjoxNDQ5NTUzMzQ5LCJpYXQiOjE0NDk1MTczNDksInYiOjAsImQiOnsiZmJfaWQiOiJnb29nbGUtb2F1dGgyfDExMjczMjM3MjI2ODg5MDM4NTIxMyJ9LCJhenAiOiJwNW9xdTF4YzRVN0E4azN2aW1XaVY3dEFwbE5MTjZaeiJ9.a4Q8D39U7RJ3cPVYOU-tSjSJlmmyY-HR2KumaRdIwqQ";
+    friendsRef.authWithCustomToken(token, function(error, auth) {
+        if (error) {
+            console.log("Authentication Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", auth);
+        }
+    });
+
+    var syncObject = $firebaseObject(friendsRef);
+    // three way data binding
+    syncObject.$bindTo($scope, 'days');
+
 
 
     $scope.reset = function() {
 
-        var friendsRef = new Firebase("https://luminous-fire-4441.firebaseio.com/days");
-        // Here we're using the Firebase Token we stored after login
-
-        var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiU2Fmd2VuIE1hcnpvdWd1aSIsImVtYWlsIjoic2Fmd2VuQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2NybS1kZW1vLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMjczMjM3MjI2ODg5MDM4NTIxMyIsImF1ZCI6InA1b3F1MXhjNFU3QThrM3ZpbVdpVjd0QXBsTkxONlp6IiwiZXhwIjoxNDQ5NDU1MjE3LCJpYXQiOjE0NDk0MTkyMTcsInYiOjAsImQiOnsiZmJfaWQiOiJnb29nbGUtb2F1dGgyfDExMjczMjM3MjI2ODg5MDM4NTIxMyJ9LCJhenAiOiJwNW9xdTF4YzRVN0E4azN2aW1XaVY3dEFwbE5MTjZaeiJ9.Y_LoyEK89W-F_r1qCGUhH-XfURVYF2Ts_bL1uMxMQuk";
-        friendsRef.authWithCustomToken(token, function(error, auth) {
-            if (error) {
-                console.log("Authentication Failed!", error);
-            } else {
-                console.log("Authenticated successfully with payload:", auth);
-            }
-        });
-
         var fb = $firebaseArray(friendsRef);
 
-        fb.$add({
+        friendsRef.set({
             monday: {
                 name: 'Monday',
                 slots: {
